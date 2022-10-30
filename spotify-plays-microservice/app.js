dc3e5cb4468294be3c42ba3831fa26768f8bac31
Eli <e.m.mills1995@gmail.com>
2022-10-30 19:48:43 -0400

    Wrote function to retrieve track given URI and send popularity in body of HTTP response to client.

--- spotify-plays-microservice/app.js
+++ spotify-plays-microservice/app.js
@@ -2,20 +2,42 @@ const express = require('express');
 const fetch = require('node-fetch');
 const app = express();
 const PORT = 3100;
+const spotify = 'https://api.spotify.com/v1';
+
+const getToken = async () => {
+    const getTokenUrl = 'http://localhost:3000/';
+    return fetch(getTokenUrl)
+    .then(res=>res.text());
+    
+}
+
+const getSongPlays = async (uri, token) => {
+    console.log(`getSongPlays called with uri=${uri} and token=${token}\n`);
+    const headers = {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+    }
+
+    return fetch(`${spotify}/tracks/${uri}`, {headers})
+    .then(track => track.json())
+    .then(track => track.popularity)
+}
 
 
 app.get('/songuri/:uri', (req,res)=>{
     console.log("Request received at song uri route.");
 
-    let token;
-    const getTokenUrl = 'http://localhost:3000';
     const uri = req.params.uri;
     
     // Get API token
-    fetch(getTokenUrl)
-    .then(res=>res.text())
-    .then(token=>{console.log(`Token: ${token}\nURI: ${uri}`);})
-    .catch(err=>console.log(`There was an error:\n${err}`));
+    getToken()
+    .then(token => getSongPlays(uri, token))
+    .then(popularity => {
+        console.log(`Popularity retrieved: ${popularity}`);
+        res.send(popularity.toString());
+    })
+    .catch(err=>console.log(`There was an error: ${err}`));
+    
 })
 
 
